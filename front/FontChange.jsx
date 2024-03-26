045e84052b57388fe3a6075e68863be7da85b751
khushigoyal387 <khushigoyal387@gmail.com>
2024-03-26 20:59:05 +0530

    first commit

--- front/FontChange.jsx
+++ front/FontChange.jsx
@@ -1,84 +1,42 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import "./FontChange.css";
 
-function FontChange() {
-  const [text, setText] = useState("");
-  const [change, setChange] = useState([]);
+function Text() {
+    const [input, setInputText] = useState("");
+    const [isBold, setIsBold] = useState(false);
+    const [isItalic, setIsItalic] = useState(false);
+    const [isLowercase, setIsLowercase] = useState(false);
+    const [isUppercase, setIsUppercase] = useState(false);
 
-  function submit(e) {
-    setText(e.target.value);
-  }
+    return (
+        <div>
+            <div className='parent'>
+                <input
+                    type="text"
+                    placeholder="Enter Something"
+                    className={`${isBold ? "bold" : ""} ${isItalic ? "italic" : ""} ${isLowercase ? "lowercase" : ""} ${isUppercase ? "uppercase" : ""}`}
+                    value={input}
+                    onChange={(e) => setInputText(e.target.value)}
+                />
 
-  function handle(e) {
-    e.preventDefault();
+                <button onClick={() => setIsBold(!isBold)} className={isBold ? "normal" : "bold"}>
+                    {isBold ? "Normal" : "Bold"}
+                </button>
 
-    if (e.target.id === "one") {
-      if (change.includes("upperCase")) {
-        setChange(change.filter((item) => item !== "upperCase"));
-      } else {
-        setChange([...change, "upperCase"]);
-      }
-    } else if (e.target.id === "two") {
-      if (change.includes("lowerCase")) {
-        setChange(change.filter((item) => item !== "lowerCase"));
-        console.log(change);
-      } else {
-        setChange([...change, "lowerCase"]);
-        console.log(change);
-      }
-    } else if (e.target.id === "three") {
-      if (change.includes("italic")) {
-        setChange(change.filter((item) => item !== "italic"));
-      } else {
-        setChange([...change, "italic"]);
-      }
-    } else if (e.target.id === "four") {
-      if (change.includes("bold")) {
-        setChange(change.filter((item) => item !== "bold"));
-      } else {
-        setChange([...change, "bold"]);
-      }
-    }
-  }
+                <button onClick={() => setIsItalic(!isItalic)} className={isItalic ? "normal" : "italic"}>
+                    {isItalic ? "Normal" : "Italic"}
+                </button>
 
-  return (
-    <>
-      <div className="font-style">
-        <div className="type">
-          <textarea
-            className={change.length > 0 ? change.join(" ") : ""}
-            name=""
-            id=""
-            cols="30"
-            rows="4"
-            placeholder="type..."
-            onChange={submit}
-          >
-            {text}
-          </textarea>
+                <button onClick={() => setIsLowercase(!isLowercase)} className={isLowercase ? "normal" : "lowercase"}>
+                    {isLowercase ? "Normal" : "Lowercase"}
+                </button>
 
+                <button onClick={() => setIsUppercase(!isUppercase)} className={isUppercase ? "normal" : "uppercase"}>
+                    {isUppercase ? "Normal" : "Uppercase"}
+                </button>
+            </div>
         </div>
-        <div className="btns">
-          <div>
-            <button id="one" onClick={handle}>
-              toUpperCase
-            </button>
-            <button id="three" onClick={handle}>
-              Italic
-            </button>
-          </div>
-          <div>
-            <button id="two" onClick={handle}>
-              toLowerCase
-            </button>
-            <button id="four" onClick={handle}>
-              Bold
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    );
 }
 
-export default FontChange;
+export default Text;
