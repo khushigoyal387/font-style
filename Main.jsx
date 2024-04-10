48f55b5163c4fa64829f76170902b173416d1c01
khushigoyal387 <khushigoyal387@gmail.com>
2024-04-10 16:15:13 +0530

    ecomerce

--- Main.jsx
+++ Main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 // import Main from "./boxes/Main"
-import FontChange from './front/FontChange';
+// import FontChange from './front/FontChange';
+// import table from "./Table/table"
+// import Home from ".PUBLIC/registration/Home"
+// import App from "./App.jsx"
+import Ecomerce from './PUBLIC/ECommerce/Ecomerce';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<FontChange/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<Ecomerce/>);
 
 
